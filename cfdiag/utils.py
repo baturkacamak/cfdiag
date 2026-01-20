b3391b6297fecbd5e58442e3564f9cc69a94f6dd
Batur Kaçamak <batisoline@gmail.com>
2026-01-20 23:01:04 +0100

    Fix: CI Artifact Action Deprecation and PyInstaller Entry Point

--- cfdiag/utils.py
+++ cfdiag/utils.py
@@ -2,7 +2,7 @@ import os
 import threading
 from typing import Dict, Any
 
-VERSION = "3.1.0"
+VERSION = "3.1.1"
 SEPARATOR = "=" * 60
 SUB_SEPARATOR = "-" * 60
 REPO_URL = "https://raw.githubusercontent.com/baturkacamak/cfdiag/main/cfdiag.py"
